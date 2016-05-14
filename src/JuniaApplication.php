c3b007be59c5402f37fdc0137084de0925f4a8e5
gossi <github@gos.si>
2016-05-14 19:03:29 +0200

    junia app

---
+++ src/JuniaApplication.php
@@ -0,0 +1,21 @@
+<?php
+namespace iuf\junia\application;
+
+use keeko\framework\foundation\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Iuf Junia-app
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class JuniaApplication extends AbstractApplication {
+
+	/**
+	 * @param Request $request
+	 * @param string $path
+	 */
+	public function run(Request $request, $path) {
+	}
+}
